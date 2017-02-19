7ba262282dcf367bd439cee75c1ad615fb854855
corbolj <lcorbo@singlestoneconsulting.com>
2017-02-19 09:41:02 -0500

    first commit

---
+++ Project_download.ps1
@@ -0,0 +1,3 @@
+git clone https://github.com/singlestone/Packer-Win10.git
+git clone https://github.com/singlestone/Packer-Win2012r2.git
+git clone https://github.com/singlestone/SSH_Test_Vagrant.git

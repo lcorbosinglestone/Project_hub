651bf1e842617733994960facc73ed276de4095e
corbolj <lcorbo@singlestoneconsulting.com>
2017-02-20 10:09:08 -0500

    Updated list

--- Project_download.ps1
+++ Project_download.ps1
@@ -1,3 +1,7 @@
 git clone https://github.com/singlestone/Packer-Win10.git
 git clone https://github.com/singlestone/Packer-Win2012r2.git
 git clone https://github.com/singlestone/SSH_Test_Vagrant.git
+git clone https://github.com/singlestone/Windows_Scripts_Examples.git
+git clone https://github.com/singlestone/Terraform_Examples.git
+git clone https://github.com/singlestone/Vagrant_DSC_CHEF_Example.git
+git clone https://github.com/singlestone/Vagrant_Linux_Examples.git
